4961c61638037c5c6bb13e17b14272cb98caa849
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2022-06-19 22:58:31 +0200

    Drawing grid

--- GUIOfGame.java
+++ GUIOfGame.java
@@ -1,12 +1,47 @@
 package com.mycompany.gameoflife;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Color;
+
 public class GUIOfGame extends javax.swing.JFrame {
+    
+    final int width = 200, height = 100;
+    
+    boolean[][] currentState = new boolean[height][width];
+    boolean[][] nextState = new boolean[height][width];
+    boolean play;
+    
+    Image offScreenImage;
+    Graphics offScreenGraphics;
+    
+    
     public GUIOfGame() {
         initComponents();
+    
+        offScreenImage = createImage(boardPanel.getWidth(), boardPanel.getHeight());
+        offScreenGraphics = offScreenImage.getGraphics();
     }
     
+    void display(){
+        offScreenGraphics.setColor(boardPanel.getBackground());
+        offScreenGraphics.fillRect(0, 0, boardPanel.getWidth(), boardPanel.getHeight());
+        
+        
+        offScreenGraphics.setColor(Color.BLACK); //rysowanie siatki
+        for(int i = 1; i < height;i++){
+            int y = i * boardPanel.getHeight()/height;
+            offScreenGraphics.drawLine(0, y, boardPanel.getWidth(), y);
+        }
+        for(int j = 1; j < width; j++){
+            int x = j * boardPanel.getWidth()/width;
+            offScreenGraphics.drawLine(x, 0, x, boardPanel.getHeight());
+        }
+        
+        boardPanel.getGraphics().drawImage(offScreenImage,0,0,boardPanel);
+    }
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         boardPanel = new javax.swing.JPanel();
@@ -16,6 +51,16 @@ public class GUIOfGame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         boardPanel.setBackground(new java.awt.Color(153, 153, 153));
+        boardPanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                boardPanelMouseClicked(evt);
+            }
+        });
+        boardPanel.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                boardPanelComponentResized(evt);
+            }
+        });
 
         javax.swing.GroupLayout boardPanelLayout = new javax.swing.GroupLayout(boardPanel);
         boardPanel.setLayout(boardPanelLayout);
@@ -60,7 +105,17 @@ public class GUIOfGame extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
+
+    private void boardPanelMouseClicked(java.awt.event.MouseEvent evt) {                                        
+        display();
+    }                                       
+
+    private void boardPanelComponentResized(java.awt.event.ComponentEvent evt) {                                            
+        offScreenImage = createImage(boardPanel.getWidth(), boardPanel.getHeight());
+        offScreenGraphics = offScreenImage.getGraphics();
+        display();
+    }                                           
 
     
     public static void main(String args[]) {
@@ -71,10 +126,10 @@ public class GUIOfGame extends javax.swing.JFrame {
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JPanel boardPanel;
     private javax.swing.JButton clearButton;
     private javax.swing.JButton startButton;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
+
 }
